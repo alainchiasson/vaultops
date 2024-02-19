acc661ea23f7ef88fff7fde4939eff40998a9c17
Alain Chiasson <alain@chiasson.org>
2024-02-19 12:05:32 -0500

    removed cli argument vault

--- vaultops/config_loader.py
+++ vaultops/config_loader.py
@@ -4,11 +4,12 @@ import os
 
 class _Config:
     def __init__(self):
-        """ Load the config file and parse the command line arguments."""
+        """ Load the config file and parse the command line arguments. File takes precedence"""
 
         # Get command line arguments first
         parser = argparse.ArgumentParser(description = 'MyApp')
         parser.add_argument('-c', '--config', help='Config file', default='config.json')
+        parser.add_argument('-v', '--vault_url', help='Vault URL')
         self.args = parser.parse_args()
         
         with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), getattr(self.args, 'config'))) as json_config_file:
