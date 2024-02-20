5a76a0154ab434d882437040084a2a54bcd54988
Alain Chiasson <alain@chiasson.org>
2024-02-20 06:29:10 -0500

    Remove debug from main

--- main.py
+++ main.py
@@ -4,33 +4,15 @@ from vaultops.vops import vops
 
 if __name__ == '__main__':
 
-    # Starting point
-    print("Config: ", vops.config)
-    print("Nodes: ", vops.nodes)
-    print("Creds: ", vops.creds)
-
     # get variables
     name = "localhost"
 
-    # add a vault nodes - refactor after
-
-    nodes = vops.nodes
+    # Add a node to be managed.
     vops.add_node(name, "http://localhost:8200")
-    print("Nodes: ", nodes.nodes)
-
-    # Confirm the node needs to be initialised.
-    node = vops.get_node(name)
-    print("Node: ", node)
-
-    # Init the nodes
     result = vops.init_node(name)
-    print("Result: ", result)
-
-    print("Nodes: ", nodes.nodes)
 
     vops.write()
 
+    # Unseal the node
     response = vops.unseal_node(name)
-    #response = client.sys.submit_unseal_keys(cred['keys'])
 
-    print(response)
